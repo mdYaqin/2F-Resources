be4b4fc0fe95a0d364bd256c8ce47fb56280fbb4
Shalahuddin Ali <shalahuddinali@Shalahuddins-MacBook-Air.local>
2025-06-06 06:10:28 +0800

    removes hardcoded data

--- src/components/admin/LoginForm.tsx
+++ src/components/admin/LoginForm.tsx
@@ -134,8 +134,8 @@ export default function LoginPage() {
           ) : (
             <>
               <p>
-                Enter your email address and we'll send you a link to reset your
-                password.
+                Enter your email address and we&apos;ll send you a link to reset
+                your password.
               </p>
               <Form onSubmit={handleForgotPassword}>
                 <Form.Group className="mb-3">
