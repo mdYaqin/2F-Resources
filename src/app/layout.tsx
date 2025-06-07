9af43485e24ff57db35f17859abf433cfe8e8dce
Shalahuddin Ali <shalahuddinali@Shalahuddins-MacBook-Air.local>
2025-06-07 13:49:37 +0800

    changed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,8 +43,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${openSans.variable} ${teko.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="2fResources" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#232121" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
