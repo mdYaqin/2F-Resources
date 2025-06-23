440674c048e4a2e966dc5744c51142e3b6ab389c
Shalahuddin Ali <shalahuddinali@Shalahuddins-MacBook-Air.local>
2025-06-23 20:10:19 +0800

    disable user from deleting self

--- src/app/api/admin/users/[id]/route.ts
+++ src/app/api/admin/users/[id]/route.ts
@@ -1,15 +1,31 @@
 import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
-  const { id } = await params;
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user?.id) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const currentUserId = session.user.id;
+  const userIdToDelete = params.id;
+
+  if (currentUserId === userIdToDelete) {
+    return NextResponse.json(
+      { error: "You cannot delete your own account." },
+      { status: 403 }
+    );
+  }
 
   try {
     const user = await prisma.user.findUnique({
-      where: { id },
+      where: { id: userIdToDelete },
     });
 
     if (!user) {
@@ -17,13 +33,12 @@ export async function DELETE(
     }
 
     await prisma.user.delete({
-      where: { id },
+      where: { id: userIdToDelete },
     });
 
     return NextResponse.json({ message: "User deleted successfully" });
   } catch (error) {
-    console.error("Error deleting User:", error);
-
+    console.error("Error deleting user:", error);
     return NextResponse.json(
       { error: "Failed to delete user" },
       { status: 500 }
